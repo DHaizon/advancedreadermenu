f89947e899af820e4a1c8fc1e21d5f69aaae1c53
Pan Zhong Qin <fps80391@gmail.com>
2026-07-02 08:55:34 -0500

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-return {
-	name = "zzz-advancedreadermenu",
-	fullname = _("Advanced Reader Menu"),
-	description = _([[An advanced reader menu with custom highlights, geometric underlines, and redesigned palettes.]]),
-	version = "v1.0.7",
-}
